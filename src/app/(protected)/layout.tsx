e2e77aa73441ad4d51c1fb392feacc8b5bace40e
Mohith1490 <mohith1754singh@gmail.com>
2025-08-14 00:55:50 +0530

    fatel issue

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,19 +1,3 @@
-import { Metadata } from "next";
-
-
-export const metadata: Metadata = {
-  title: "Dashboard",
-  description: "Developed by SIDA Technologies",
-};
-
-export default function DashboardLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return (
-        <>
-            {children}
-        </>
-    )
+export default function BookingLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
 }
